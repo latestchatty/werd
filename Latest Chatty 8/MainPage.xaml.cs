a78627bd97bb0960cbbfd4c1273e1b62d402704f
Willie Zutz <boarder2@bit-shift.com>
2012-10-27 16:23:32 -0600

    First commit! Here's the mess as it is right now. News stories load on the main view. Lots of code pulled directly from the WP7 app.  I'll be going through it and adapting it to work with async for WinRT and all the other fun new stuff we can use.

---
+++ Latest Chatty 8/MainPage.xaml.cs
@@ -0,0 +1,88 @@
+﻿using Latest_Chatty_8.Data;
+using Latest_Chatty_8.DataModel;
+using Latest_Chatty_8.Networking;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Display;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Items Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234233
+
+namespace Latest_Chatty_8
+{
+	/// <summary>
+	/// A page that displays a collection of item previews.  In the Split Application this page
+	/// is used to display and select one of the available groups.
+	/// </summary>
+	public sealed partial class MainPage : Latest_Chatty_8.Common.LayoutAwarePage
+	{
+		private readonly ObservableCollection<NewsStory> stories;
+		public MainPage()
+		{
+			this.InitializeComponent();
+			this.stories = new ObservableCollection<NewsStory>();
+		}
+
+		/// <summary>
+		/// Populates the page with content passed during navigation.  Any saved state is also
+		/// provided when recreating a page from a prior session.
+		/// </summary>
+		/// <param name="navigationParameter">The parameter value passed to
+		/// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested.
+		/// </param>
+		/// <param name="pageState">A dictionary of state preserved by this page during an earlier
+		/// session.  This will be null the first time a page is visited.</param>
+		protected override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
+		{
+			if (pageState == null)
+			{
+				//Load data from scratch.
+				CoreServices.Instance.QueueDownload(Locations.Stories, GotNewsStories);
+			}
+		}
+
+		private void GotNewsStories(XDocument d)
+		{
+			this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.High, () =>
+				{
+					this.stories.Clear();
+					var retrievedStories = from s in d.Descendants("story")
+												  select new NewsStory(s);
+
+					foreach (var story in retrievedStories)
+					{
+						this.stories.Add(story);
+					}
+
+					this.DefaultViewModel["Items"] = this.stories;
+				});
+		}
+
+		/// <summary>
+		/// Invoked when an item is clicked.
+		/// </summary>
+		/// <param name="sender">The GridView (or ListView when the application is snapped)
+		/// displaying the item clicked.</param>
+		/// <param name="e">Event data that describes the item clicked.</param>
+		void ItemView_ItemClick(object sender, ItemClickEventArgs e)
+		{
+			// Navigate to the appropriate destination page, configuring the new page
+			// by passing required information as a navigation parameter
+			var groupId = ((SampleDataGroup)e.ClickedItem).UniqueId;
+			this.Frame.Navigate(typeof(SplitPage), groupId);
+		}
+	}
+}
