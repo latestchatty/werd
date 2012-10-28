e5a0270ac5a2b402ca7c027fbf3043d0fe4de7f1
Willie Zutz <boarder2@gmail.com>
2012-10-28 02:35:10 -0600

    Adding comment parsing and viewing. Trying to figure out how to get the main page to scroll when there's more content than will fit.
    ... This seems more difficult than it should be.
    
    Maybe because it's 2:30AM.  I should be sleeping.  This is going to screw up my whole week.

--- Latest Chatty 8/Networking/Locations.cs
+++ Latest Chatty 8/Networking/Locations.cs
@@ -12,7 +12,11 @@ namespace Latest_Chatty_8.Networking
 		public const string ServiceHost = "http://shackapi.stonedonkey.com/";
 		public const string PostUrl = ServiceHost + "post/";
 		public const string Stories = ServiceHost + "stories.json";
-
+		public const string ChattyComments = ServiceHost + "chatty/index.json";
+		public static string MakeCommentUrl(int commentId)
+		{
+			return string.Format("{0}/thread/{1}.json", ServiceHost, commentId);
+		}
 		#endregion
 	}
 }
