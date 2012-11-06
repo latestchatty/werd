69cc488b83c8fe3b849c882f0d424bb3dde4313d
Willie Zutz <boarder2@gmail.com>
2012-11-06 00:36:29 -0700

    Fixing various compile warnings.

--- Latest Chatty 8/Views/Chatty.xaml.cs
+++ Latest Chatty 8/Views/Chatty.xaml.cs
@@ -47,7 +47,7 @@ namespace Latest_Chatty_8.Views
 		/// </param>
 		/// <param name="pageState">A dictionary of state preserved by this page during an earlier
 		/// session.  This will be null the first time a page is visited.</param>
-		async protected override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
+		protected override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
 		{
 			var skipSavedLoad = ((!CoreServices.Instance.ReturningFromThreadView) && (!CoreServices.Instance.PostedAComment));
 			CoreServices.Instance.ReturningFromThreadView = false;
@@ -103,7 +103,7 @@ namespace Latest_Chatty_8.Views
 			this.UnsetLoading();
 		}
 
-		async void ChattyCommentListSelectionChanged(object sender, SelectionChangedEventArgs e)
+		void ChattyCommentListSelectionChanged(object sender, SelectionChangedEventArgs e)
 		{
 			this.GetSelectedThread();
 		}
