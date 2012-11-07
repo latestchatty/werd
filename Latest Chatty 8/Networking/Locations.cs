891c933e46769fdf17bda88fea58d371f0174933
Willie Zutz <boarder2@gmail.com>
2012-11-06 18:07:10 -0700

    Changed the layout of the Send button on the reply based on which side of the screen it's snapped to. Fixed the bug where refreshing wouldn't pick up user name changes for replies/my posts.

--- Latest Chatty 8/Networking/Locations.cs
+++ Latest Chatty 8/Networking/Locations.cs
@@ -13,8 +13,14 @@ namespace Latest_Chatty_8.Networking
 		public const string PostUrl = ServiceHost + "post/";
 		public const string Stories = ServiceHost + "stories.json";
 		public const string ChattyComments = ServiceHost + "chatty/index.json";
-		public static string ReplyComments = ServiceHost + "Search.json/?parent_author=" + CoreServices.Instance.Credentials.UserName;
-		public static string MyComments = ServiceHost + "Search.json/?Author=" + CoreServices.Instance.Credentials.UserName;
+		public static string ReplyComments
+		{
+			get { return ServiceHost + "Search.json/?parent_author=" + CoreServices.Instance.Credentials.UserName; }
+		}
+		public static string MyComments
+		{
+			get { return ServiceHost + "Search.json/?Author=" + CoreServices.Instance.Credentials.UserName; }
+		}
 
 		public static string MakeCommentUrl(int commentId)
 		{
