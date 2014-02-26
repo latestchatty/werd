1890fa027333aacc8f3e8f3a8420ded839ec3be8
Willie Zutz <boarder2@bit-shift.com>
2014-02-26 00:42:36 -0700

    Now it's really a start.

---
+++ Latest Chatty 8/Views/InlineThreadView.xaml.cs
@@ -0,0 +1,64 @@
+﻿using Latest_Chatty_8.Common;
+using Latest_Chatty_8.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Latest_Chatty_8.Views
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class InlineThreadView : LayoutAwarePage
+	{
+		private ObservableCollection<Comment> chattyComments;
+
+		public InlineThreadView()
+		{
+			this.InitializeComponent();
+			this.chattyComments = new ObservableCollection<Comment>();
+			this.DefaultViewModel["Comments"] = this.chattyComments;
+		}
+
+		protected override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
+		{
+			int threadId = 0;
+			int selectedCommentId = 0;
+			List<Comment> comment = null;
+
+			var navComment = navigationParameter as Comment;
+			//If we have the entire comment passed as the navigation parameter, we can load the whole thing without hitting the interwebs.
+			if (navComment != null)
+			{
+				var entireComment = navComment.FlattenedComments.ToList();
+				threadId = navComment.Id;
+				selectedCommentId = threadId;
+				comment = entireComment;
+				CoreServices.Instance.PostCounts[threadId] = navComment.ReplyCount;
+				navComment.HasNewReplies = false; //Viewed it, no longer has new replies.
+				navComment.IsNew = false; //Viewed it, mark it as such.
+				this.DefaultViewModel["Comments"] = comment;
+				//this.commentList.ItemsSource = comment;
+			}
+			else
+			{
+				threadId = (int)navigationParameter;
+				selectedCommentId = threadId;
+			}
+		}
+	}
+}
