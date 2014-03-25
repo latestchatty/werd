ce3f80f27ddc87cca7adc078ebec2a1000640e92
Willie Zutz <boarder2@gmail.com>
2014-03-25 01:16:07 -0600

    Starting on pinned posts. Definitely have some timing issues to work out. Threading is hard.

--- Latest Chatty 8/Settings/LatestChattySettings.cs
+++ Latest Chatty 8/Settings/LatestChattySettings.cs
@@ -15,6 +15,7 @@ using Latest_Chatty_8.Networking;
 using Newtonsoft.Json.Linq;
 using LatestChatty.Settings;
 using Windows.UI.Xaml;
+using System.IO;
 
 namespace Latest_Chatty_8.Settings
 {
@@ -151,36 +152,102 @@ namespace Latest_Chatty_8.Settings
 
 		public void CreateInstance() { }
 
+		private string clientSessionToken;
+
+		#region WinChatty Service
+		async private Task RefreshClientToken()
+		{
+			if (CoreServices.Instance.Credentials != null)
+			{
+				var data = POSTHelper.BuildDataString(new Dictionary<string, string> {
+					{"username", CoreServices.Instance.Credentials.UserName},
+					{"password", CoreServices.Instance.Credentials.Password},
+					{"client", "latestchatty8"},
+					{"version", "1"}
+				});
+				var response = await POSTHelper.Send(Locations.GetClientSessionToken, data, false);
+				using (var reader = new StreamReader(response.GetResponseStream()))
+				{
+					var responseData = await reader.ReadToEndAsync();
+					var parsedResponse = JToken.Parse(responseData);
+					var sessionToken = parsedResponse["clientSessionToken"].ToString();
+					if (!string.IsNullOrWhiteSpace(sessionToken))
+					{
+						this.clientSessionToken = sessionToken;
+						System.Diagnostics.Debug.WriteLine("Client Session Token refreshed - new value is {0}", this.clientSessionToken);
+					}
+				}
+			}
+		}
+
+		async public Task<IEnumerable<int>> GetPinnedPostIds()
+		{
+			var pinnedPostIds = new List<int>();
+			var data = POSTHelper.BuildDataString(new Dictionary<string, string> { { "clientSessionToken", this.clientSessionToken } });
+			var response = await POSTHelper.Send(Locations.GetMarkedPosts, data, false);
+			using (var reader = new StreamReader(response.GetResponseStream()))
+			{
+				var responseData = await reader.ReadToEndAsync();
+				var parsedResponse = JToken.Parse(responseData);
+				foreach (var post in parsedResponse["markedPosts"].Children())
+				{
+					pinnedPostIds.Add((int)post["id"]);
+				}
+			}
+			return pinnedPostIds;
+		}
+
+		async private Task MarkThread(int id, string type)
+		{
+			var data = POSTHelper.BuildDataString(new Dictionary<string, string> {
+				{ "clientSessionToken", this.clientSessionToken },
+				{ "postId", id.ToString() },
+				{ "type", type}
+			});
+			var t = await POSTHelper.Send(Locations.MarkPost, data, false);
+		}
+		async public Task PinThread(int id)
+		{
+			await this.MarkThread(id, "pinned");
+		}
+
+		async public Task UnPinThread(int id)
+		{
+			await this.MarkThread(id, "unmarked");
+		}
+		#endregion
+
 		public async Task LoadLongRunningSettings()
 		{
 			try
 			{
 				this.loadingSettingsInternal = true;
+				await this.RefreshClientToken();
 				if (!this.CloudSync)
 				{
 					this.pinnedThreadIds = await ComplexSetting.ReadSetting<List<int>>(pinnedThreads);
 				}
 				else
 				{
-					var json = await JSONDownloader.Download(Locations.MyCloudSettings);
-					if (json != null)
-					{
-						this.cloudSettings = (JObject)json;
-						this.AutoCollapseInformative = json[autocollapseinformative] != null ? (bool)json[autocollapseinformative] : false;
-						this.AutoCollapseInteresting = json[autocollapseinteresting] != null ? (bool)json[autocollapseinteresting] : false;
-						this.AutoCollapseNews = json[autocollapsenews] != null ? (bool)json[autocollapsenews] : false;
-						this.AutoCollapseNws = json[autocollapsenws] != null ? (bool)json[autocollapsenws] : true;
-						this.AutoCollapseOffTopic = json[autocollapseofftopic] != null ? (bool)json[autocollapseofftopic] : false;
-						this.AutoCollapsePolitical = json[autocollapsepolitical] != null ? (bool)json[autocollapsepolitical] : false;
-						this.AutoCollapseStupid = json[autocollapsestupid] != null ? (bool)json[autocollapsestupid] : false;
-						this.ShowInlineImages = json[showInlineImages] != null ? (bool)json[showInlineImages] : true;
-						this.AutoPinOnReply = (json[autopinonreply] != null) ? (bool)json[autopinonreply] : false;
-						this.AutoRemoveOnExpire = (json[autoremoveonexpire] != null) ? (bool)json[autoremoveonexpire] : false;
-						this.SortNewToTop = (json[sortNewToTop] != null) ? (bool)json[sortNewToTop] : false;
-						this.SplitPercent = (json[splitpercent] != null) ? (int)json[splitpercent] : 45;
+					//var json = await JSONDownloader.Download(Locations.MyCloudSettings);
+					//if (json != null)
+					//{
+					//	this.cloudSettings = (JObject)json;
+					//	this.AutoCollapseInformative = json[autocollapseinformative] != null ? (bool)json[autocollapseinformative] : false;
+					//	this.AutoCollapseInteresting = json[autocollapseinteresting] != null ? (bool)json[autocollapseinteresting] : false;
+					//	this.AutoCollapseNews = json[autocollapsenews] != null ? (bool)json[autocollapsenews] : false;
+					//	this.AutoCollapseNws = json[autocollapsenws] != null ? (bool)json[autocollapsenws] : true;
+					//	this.AutoCollapseOffTopic = json[autocollapseofftopic] != null ? (bool)json[autocollapseofftopic] : false;
+					//	this.AutoCollapsePolitical = json[autocollapsepolitical] != null ? (bool)json[autocollapsepolitical] : false;
+					//	this.AutoCollapseStupid = json[autocollapsestupid] != null ? (bool)json[autocollapsestupid] : false;
+					//	this.ShowInlineImages = json[showInlineImages] != null ? (bool)json[showInlineImages] : true;
+					//	this.AutoPinOnReply = (json[autopinonreply] != null) ? (bool)json[autopinonreply] : false;
+					//	this.AutoRemoveOnExpire = (json[autoremoveonexpire] != null) ? (bool)json[autoremoveonexpire] : false;
+					//	this.SortNewToTop = (json[sortNewToTop] != null) ? (bool)json[sortNewToTop] : false;
+					//	this.SplitPercent = (json[splitpercent] != null) ? (int)json[splitpercent] : 45;
 
-						this.pinnedThreadIds = json["watched"].Children().Select(c => (int)c).ToList<int>();
-					}
+					//	this.pinnedThreadIds = json["watched"].Children().Select(c => (int)c).ToList<int>();
+					//}
 				}
 			}
 			catch (WebException e)
@@ -197,10 +264,6 @@ namespace Latest_Chatty_8.Settings
 			}
 			finally
 			{
-				if (this.pinnedThreadIds == null)
-				{
-					this.pinnedThreadIds = new List<int>();
-				}
 				this.loadingSettingsInternal = false;
 			}
 		}
@@ -210,97 +273,58 @@ namespace Latest_Chatty_8.Settings
 			return this.pinnedThreadIds.Contains(id);
 		}
 
-		public async Task RefreshPinnedThreads()
+		public async Task SaveToCloud()
 		{
-			//var cToRemove = new List<Comment>();
-			//var commentsToAdd = new List<Comment>();
-			//foreach (var pinnedItemId in this.pinnedThreadIds)
+			//try
 			//{
-			//	var c = await CommentDownloader.GetComment((int)pinnedItemId, false);
-
-			//	// If this is null there was a problem getting a comment.
-			//	// This can happen for more than one reason.  Either we have a temporary problem, in which case we'll want to try again later...
-			//	// Or this thread was nuked or there's a problem with the API and it's blowing up... in which case we'll want to remove this
-			//	//  so we don't try to eternally retrieve it....
-			//	// Right now I don't have a great way to determine which case it is.
-			//	// TODO: Consider adding a failure count, pinned thread expiration date, something like that?
-			//	if (c != null)
+			//	//If cloud sync is enabled
+			//	if (!this.loadingSettingsInternal && LatestChattySettings.Instance.CloudSync)
 			//	{
-			//		if (LatestChattySettings.instance.AutoRemoveOnExpire && (c.Date.AddHours(18).ToUniversalTime() < DateTime.UtcNow))
+			//		System.Diagnostics.Debug.WriteLine("Syncing to cloud...");
+			//		//If we don't have settings already, create them.
+			//		if (this.cloudSettings == null)
 			//		{
-			//			cToRemove.Add(c);
+			//			this.cloudSettings = new JObject(
+			//					  new JProperty("watched",
+			//							new JArray(this.pinnedThreadIds)
+			//							),
+			//					  new JProperty(showInlineImages, this.ShowInlineImages),
+			//					  new JProperty(autocollapseinformative, this.AutoCollapseInformative),
+			//					  new JProperty(autocollapseinteresting, this.AutoCollapseInteresting),
+			//					  new JProperty(autocollapsenews, this.AutoCollapseNews),
+			//					  new JProperty(autocollapsenws, this.AutoCollapseNws),
+			//					  new JProperty(autocollapseofftopic, this.AutoCollapseOffTopic),
+			//					  new JProperty(autocollapsepolitical, this.AutoCollapsePolitical),
+			//					  new JProperty(autocollapsestupid, this.AutoCollapseStupid),
+			//					  new JProperty(autopinonreply, this.AutoPinOnReply),
+			//					  new JProperty(autoremoveonexpire, this.AutoRemoveOnExpire),
+			//					  new JProperty(sortNewToTop, this.SortNewToTop),
+			//									new JProperty(splitpercent, this.SplitPercent));
 			//		}
 			//		else
 			//		{
-			//			commentsToAdd.Add(c);
+			//			//If we do have settings, use them.
+			//			this.cloudSettings.CreateOrSet("watched", new JArray(this.pinnedThreadIds));
+			//			this.cloudSettings.CreateOrSet(showInlineImages, this.ShowInlineImages);
+			//			this.cloudSettings.CreateOrSet(autocollapseinformative, this.AutoCollapseInformative);
+			//			this.cloudSettings.CreateOrSet(autocollapseinteresting, this.AutoCollapseInteresting);
+			//			this.cloudSettings.CreateOrSet(autocollapsenews, this.AutoCollapseNews);
+			//			this.cloudSettings.CreateOrSet(autocollapsenws, this.AutoCollapseNws);
+			//			this.cloudSettings.CreateOrSet(autocollapseofftopic, this.AutoCollapseOffTopic);
+			//			this.cloudSettings.CreateOrSet(autocollapsepolitical, this.AutoCollapsePolitical);
+			//			this.cloudSettings.CreateOrSet(autocollapsestupid, this.AutoCollapseStupid);
+			//			this.cloudSettings.CreateOrSet(autopinonreply, this.AutoPinOnReply);
+			//			this.cloudSettings.CreateOrSet(autoremoveonexpire, this.AutoRemoveOnExpire);
+			//			this.cloudSettings.CreateOrSet(sortNewToTop, this.SortNewToTop);
+			//			this.cloudSettings.CreateOrSet(splitpercent, this.SplitPercent);
 			//		}
+			//		await POSTHelper.Send(Locations.MyCloudSettings, this.cloudSettings.ToString(), true);
 			//	}
 			//}
-
-			//foreach (var cr in cToRemove)
+			//catch
 			//{
-			//	Instance.RemovePinnedThread(cr);
+			//	System.Diagnostics.Debug.Assert(false);
 			//}
-
-			//this.pinnedThreadsCollection.Clear();
-			//foreach (var c in commentsToAdd)
-			//{
-			//	this.pinnedThreadsCollection.Add(c);
-			//}
-		}
-
-		public async Task SaveToCloud()
-		{
-			try
-			{
-				//If cloud sync is enabled
-				if (!this.loadingSettingsInternal && LatestChattySettings.Instance.CloudSync)
-				{
-					System.Diagnostics.Debug.WriteLine("Syncing to cloud...");
-					//If we don't have settings already, create them.
-					if (this.cloudSettings == null)
-					{
-						this.cloudSettings = new JObject(
-								  new JProperty("watched",
-										new JArray(this.pinnedThreadIds)
-										),
-								  new JProperty(showInlineImages, this.ShowInlineImages),
-								  new JProperty(autocollapseinformative, this.AutoCollapseInformative),
-								  new JProperty(autocollapseinteresting, this.AutoCollapseInteresting),
-								  new JProperty(autocollapsenews, this.AutoCollapseNews),
-								  new JProperty(autocollapsenws, this.AutoCollapseNws),
-								  new JProperty(autocollapseofftopic, this.AutoCollapseOffTopic),
-								  new JProperty(autocollapsepolitical, this.AutoCollapsePolitical),
-								  new JProperty(autocollapsestupid, this.AutoCollapseStupid),
-								  new JProperty(autopinonreply, this.AutoPinOnReply),
-								  new JProperty(autoremoveonexpire, this.AutoRemoveOnExpire),
-								  new JProperty(sortNewToTop, this.SortNewToTop),
-												new JProperty(splitpercent, this.SplitPercent));
-					}
-					else
-					{
-						//If we do have settings, use them.
-						this.cloudSettings.CreateOrSet("watched", new JArray(this.pinnedThreadIds));
-						this.cloudSettings.CreateOrSet(showInlineImages, this.ShowInlineImages);
-						this.cloudSettings.CreateOrSet(autocollapseinformative, this.AutoCollapseInformative);
-						this.cloudSettings.CreateOrSet(autocollapseinteresting, this.AutoCollapseInteresting);
-						this.cloudSettings.CreateOrSet(autocollapsenews, this.AutoCollapseNews);
-						this.cloudSettings.CreateOrSet(autocollapsenws, this.AutoCollapseNws);
-						this.cloudSettings.CreateOrSet(autocollapseofftopic, this.AutoCollapseOffTopic);
-						this.cloudSettings.CreateOrSet(autocollapsepolitical, this.AutoCollapsePolitical);
-						this.cloudSettings.CreateOrSet(autocollapsestupid, this.AutoCollapseStupid);
-						this.cloudSettings.CreateOrSet(autopinonreply, this.AutoPinOnReply);
-						this.cloudSettings.CreateOrSet(autoremoveonexpire, this.AutoRemoveOnExpire);
-						this.cloudSettings.CreateOrSet(sortNewToTop, this.SortNewToTop);
-						this.cloudSettings.CreateOrSet(splitpercent, this.SplitPercent);
-					}
-					await POSTHelper.Send(Locations.MyCloudSettings, this.cloudSettings.ToString(), true);
-				}
-			}
-			catch
-			{
-				System.Diagnostics.Debug.Assert(false);
-			}
 		}
 
 		internal async void Resume()
@@ -485,7 +509,7 @@ namespace Latest_Chatty_8.Settings
 		async private void CloudSyncEnabled()
 		{
 			await this.LoadLongRunningSettings();
-			await this.RefreshPinnedThreads();
+			//await this.RefreshPinnedThreads();
 		}
 
 		public DateTime LastCloudSyncTimeUtc
@@ -638,39 +662,6 @@ namespace Latest_Chatty_8.Settings
 			{
 				this.settingsContainer.Values[password] = value;
 				this.NotifyPropertyChange();
-			}
-		}
-
-		public void AddPinnedThread(CommentThread thread)
-		{
-			if (!this.pinnedThreadIds.Contains(thread.Id))
-			{
-				this.pinnedThreadIds.Add(thread.Id);
-				this.pinnedThreadsCollection.Add(thread);
-				var t = this.SaveToCloud();
-			}
-		}
-
-		public void RemovePinnedThread(CommentThread thread)
-		{
-			if (this.pinnedThreadIds.Contains(thread.Id))
-			{
-				var threadToRemove = this.pinnedThreadsCollection.SingleOrDefault(c => c.Id == thread.Id);
-				if (threadToRemove != null)
-				{
-					this.pinnedThreadsCollection.Remove(threadToRemove);
-				}
-				this.pinnedThreadIds.Remove(thread.Id);
-				var t = this.SaveToCloud();
-			}
-		}
-
-		public void ClearPinnedThreads()
-		{
-			this.pinnedThreadIds.Clear();
-			for (int i = this.pinnedThreadsCollection.Count - 1; i >= 0; i--)
-			{
-				this.pinnedThreadsCollection.RemoveAt(i);
 			}
 		}
 
