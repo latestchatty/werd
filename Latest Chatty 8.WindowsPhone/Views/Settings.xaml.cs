a16f2ea524678988c9a077f9a8a648dad1991bc0
Willie Zutz <boarder2@bt-shift.com>
2014-04-21 01:08:07 -0600

    Moved login to shared control.  Made it use Electroly's server instead of shacknews.  Shack seemed broken with complex pw's.  Either way, code's simpler.
    Added settings screen to phone app and put login there.
    
    Pinning buttons function when logged in.

---
+++ Latest Chatty 8.WindowsPhone/Views/Settings.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Latest_Chatty_8.Shared.Settings;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
+
+namespace Latest_Chatty_8.Views
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class Settings : Page
+	{
+		public Settings()
+		{
+			this.InitializeComponent();
+			Windows.Phone.UI.Input.HardwareButtons.BackPressed += HardwareButtons_BackPressed;
+		}
+
+		/// <summary>
+		/// Invoked when this page is about to be displayed in a Frame.
+		/// </summary>
+		/// <param name="e">Event data that describes how this page was reached.
+		/// This parameter is typically used to configure the page.</param>
+		protected override void OnNavigatedTo(NavigationEventArgs e)
+		{
+			this.DataContext = LatestChattySettings.Instance;
+		}
+
+		private void HardwareButtons_BackPressed(object sender, Windows.Phone.UI.Input.BackPressedEventArgs e)
+		{
+			Frame frame = Window.Current.Content as Frame;
+			if (frame == null)
+			{
+				return;
+			}
+
+			if (frame.CanGoBack)
+			{
+				frame.GoBack();
+				e.Handled = true;
+			}
+		}
+	}
+}
