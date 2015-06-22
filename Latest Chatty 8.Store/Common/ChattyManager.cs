2a5224d060fd13b989b57f84695a1dea717ce840
Willie <boarder2@bit-shift.com>
2015-06-22 14:05:32 -0600

    Frame caching and navigation stack improvements.

--- Latest Chatty 8.Store/Common/ChattyManager.cs
+++ Latest Chatty 8.Store/Common/ChattyManager.cs
@@ -17,7 +17,7 @@ using Windows.UI.Core;
 
 namespace Latest_Chatty_8.Common
 {
-	public class ChattyManager : BindableBase
+	public class ChattyManager : BindableBase, IDisposable
 	{
 		//TODO: IDisposable and free SeenPostsManger
 
@@ -455,6 +455,47 @@ namespace Latest_Chatty_8.Common
 			{
 				this.ChattyLock.Release();
 			}
+		}
+
+		#endregion
+
+		#region IDisposable Support
+		private bool disposedValue = false; // To detect redundant calls
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!disposedValue)
+			{
+				if (disposing)
+				{
+					this.ChattyLock.Dispose();
+					if (this.chattyRefreshTimer != null)
+					{
+						this.chattyRefreshTimer.Dispose();
+						this.chattyRefreshTimer = null;
+					}
+				}
+
+				// TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+				// TODO: set large fields to null.
+
+				disposedValue = true;
+			}
+		}
+
+		// TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+		// ~ChattyManager() {
+		//   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+		//   Dispose(false);
+		// }
+
+		// This code added to correctly implement the disposable pattern.
+		public void Dispose()
+		{
+			// Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+			Dispose(true);
+			// TODO: uncomment the following line if the finalizer is overridden above.
+			// GC.SuppressFinalize(this);
 		}
 		#endregion
 	}
