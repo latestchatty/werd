c1358a53419c75dff0987871b4ad86225ecf80ce
Willie <boarder2@bit-shift.com>
2015-07-27 16:01:41 -0600

    Threads won't be automatically added/removed from filtered views - to prevent things from shifting around. Seen posts limit reduced to 6500 to work around a bug in FormUrlEncodedContent that limits payload to 64K.

--- Latest Chatty 8.Store/Common/SeenPostsManager.cs
+++ Latest Chatty 8.Store/Common/SeenPostsManager.cs
@@ -72,17 +72,20 @@ namespace Latest_Chatty_8.Common
 				System.Diagnostics.Debug.WriteLine("SyncSeenPosts - Enter");
 				
 				System.Diagnostics.Debug.WriteLine("SyncSeenPosts - Getting cloud seen for merge.");
-				var cloudSeen = await this.settings.GetCloudSetting<List<int>>("SeenPosts");
+				var cloudSeen = await this.settings.GetCloudSetting<List<int>>("SeenPosts") ?? new List<int>();
 
 				if (await this.locker.WaitAsync(10))
 				{
 					lockSucceeded = true;
 					System.Diagnostics.Debug.WriteLine("SyncSeenPosts - Persisting...");
 					this.SeenPosts = this.SeenPosts.Union(cloudSeen).ToList();
+
 					//OPTIMIZE: At some point we could look through the chatty to see if an id is still active, but right now that seems like a lot of time to tie up the locker that's unecessary.
-					if (this.SeenPosts.Count > 9000)
+					if (this.SeenPosts.Count > 6800)
 					{
-						this.SeenPosts = this.SeenPosts.OrderByDescending(x => x).Skip(this.SeenPosts.Count - 9000) as List<int>;
+						//There is currently a limit in FormUrlEncodedContent of 64K.  We need to keep our payload below that.
+						//I didn't do exact math but 7000 worked, so we'll go with 6800 to save room for username and blah blah.  6800 should still be easily more posts than we ever see in a day.
+						this.SeenPosts = this.SeenPosts.OrderBy(x => x).Skip(this.SeenPosts.Count - 6800).ToList();
 					}
 
 					if (fireUpdate)
