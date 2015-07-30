2e25cb77d7b7c1fb21136bbbaebc26e52ddb99da
Willie <boarder2@bit-shift.com>
2015-07-30 00:56:59 -0600

    Added thread life color indicator to root chatty posts.  Not sure about the implementation yet.  Might need more work.

---
+++ Latest Chatty 8.Store/Converters/ThreadLifeColorConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+
+namespace Latest_Chatty_8.Converters
+{
+	public class ThreadLifeColorConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, string language)
+		{
+			if (value == null) return Color.FromArgb(255, 0, 0, 0);
+			var percent = System.Convert.ToInt32(value);
+			if (percent > 100 || percent < 0) throw new ArgumentOutOfRangeException();
+
+			var v = (byte)(255 - (percent * 1.9));
+			return Color.FromArgb(255, v, v, v);
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
