5aaa595bab7bb1740f6dcda71533259e7e42c2e6
Willie Zutz <boarder2@bit-shift.com>
2015-08-05 16:48:26 -0600

    Improved sorting code. Added sorting by having new replies and having participated.

--- Latest Chatty 8.Store/DataModel/ChattySortType.cs
+++ Latest Chatty 8.Store/DataModel/ChattySortType.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Latest_Chatty_8.DataModel
+﻿namespace Latest_Chatty_8.DataModel
 {
 	public enum ChattySortType
 	{
 		Default,
 		Inf,
 		Lol,
-		ReplyCount
+		ReplyCount,
+		HasNewReplies,
+		Participated,
 	}
 }
