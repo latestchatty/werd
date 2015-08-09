76c92a14aa3756573ff77e28ecf8c84d343ed369
Willie Zutz <boarder@gmail.com>
2015-08-09 14:31:34 -0600

    Submitting for 2.0 release.

--- Latest Chatty 8.Store/Properties/AssemblyInfo.cs
+++ Latest Chatty 8.Store/Properties/AssemblyInfo.cs
@@ -2,26 +2,26 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Latest Chatty 8")]
+[assembly: AssemblyTitle("Latest Chatty")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Latest Chatty 8")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCompany("Bit Shift Software, LLC")]
+[assembly: AssemblyProduct("Latest Chatty")]
+[assembly: AssemblyCopyright("Copyright ©  2012 - 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.0.*")]
