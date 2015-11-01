be631312bc354a776c40c206e19eae0a08af55d4
Willie Zutz <boarder2@gmail.com>
2015-11-01 11:58:59 -0700

    Changed tag view to use the inline comment view and load a thread just to see if things work - they do, sort of.

--- Latest Chatty 8.Store/Views/TagView.xaml.cs
+++ Latest Chatty 8.Store/Views/TagView.xaml.cs
@@ -1,13 +1,23 @@
-﻿using System;
+﻿using Autofac;
+using Latest_Chatty_8.Common;
+using Latest_Chatty_8.Networking;
+using Latest_Chatty_8.Settings;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Navigation;
 
 namespace Latest_Chatty_8.Views
 {
 	public sealed partial class TagView : ShellView
 	{
+		private SeenPostsManager seenPostsManager;
+		private AuthenticationManager authManager;
+		private LatestChattySettings settings;
+		private ThreadMarkManager markManager;
+
 		public override string ViewTitle
 		{
 			get { return "Tags"; }
@@ -18,6 +28,18 @@ namespace Latest_Chatty_8.Views
 			this.InitializeComponent();
 		}
 
-
+		async protected override void OnNavigatedTo(NavigationEventArgs e)
+		{
+			base.OnNavigatedTo(e);
+			var container = e.Parameter as Autofac.IContainer;
+			this.seenPostsManager = container.Resolve<SeenPostsManager>();
+			this.authManager = container.Resolve<AuthenticationManager>();
+			this.settings = container.Resolve<LatestChattySettings>();
+			this.markManager = container.Resolve<ThreadMarkManager>();
+			this.singleThreadControl.Initialize(container);
+			var commentThread = await JSONDownloader.Download(Networking.Locations.GetThread + "?id=" + "34139993");
+			var parsedThread = (await CommentDownloader.ParseThread(commentThread["threads"][0], 0, this.seenPostsManager, this.authManager, this.settings, this.markManager));
+			this.singleThreadControl.DataContext = parsedThread;
+		}
 	}
 }
