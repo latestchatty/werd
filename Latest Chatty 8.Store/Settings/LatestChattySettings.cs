b698632d338c2c24e5d5894b830ed4c5ff130f39
Willie Zutz <boarder2@bit-shift.com>
2016-01-03 01:10:59 -0700

    Updated what's new message for previous commit.

--- Latest Chatty 8.Store/Settings/LatestChattySettings.cs
+++ Latest Chatty 8.Store/Settings/LatestChattySettings.cs
@@ -590,6 +590,7 @@ namespace Latest_Chatty_8.Settings
 			{
 				return @"New in version " + this.currentVersion + Environment.NewLine + @"
 • Tap on a tag (lol, inf, etc) count to see who tagged that post
+• Added navigation buttons in thread view
 • Minor bug fixes, performance improvements, misc changes
 ";
 			}
