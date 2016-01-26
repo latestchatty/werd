e8bcb1d98dcef6989603e4cafebe406733b0e9d8
Willie Zutz <boarder2@bit-shift.com>
2016-01-26 00:15:39 -0700

    Adding original image sources and batch file to create svg

---
+++ Latest Chatty 8.Store/Assets/CreateIconFiles.bat
@@ -0,0 +1,63 @@
+
+set inkscapeExe=C:\tools\inkscape\inkscape.exe
+
+set svgPath=LogoSquare50.svg
+set size=150
+call %inkscapeExe% -f "%svgPath%" -w %size% -h %size% -e "Logo.scale-100.png"
+set size=188
+call %inkscapeExe% -f "%svgPath%" -w %size% -h %size% -e "Logo.scale-125.png"
+set size=225
+call %inkscapeExe% -f "%svgPath%" -w %size% -h %size% -e "Logo.scale-150.png"
+set size=300
+call %inkscapeExe% -f "%svgPath%" -w %size% -h %size% -e "Logo.scale-200.png"
+set size=600
+call %inkscapeExe% -f "%svgPath%" -w %size% -h %size% -e "Logo.scale-400.png"
+
+set size=310
+call %inkscapeExe% -f "%svgPath%" -w %size% -h %size% -e "BigLogo.scale-100.png"
+set size=388
+call %inkscapeExe% -f "%svgPath%" -w %size% -h %size% -e "BigLogo.scale-125.png"
+set size=465
+call %inkscapeExe% -f "%svgPath%" -w %size% -h %size% -e "BigLogo.scale-150.png"
+set size=620
+call %inkscapeExe% -f "%svgPath%" -w %size% -h %size% -e "BigLogo.scale-200.png"
+set size=1240
+call %inkscapeExe% -f "%svgPath%" -w %size% -h %size% -e "BigLogo.scale-400.png"
+
+set svgPath=LogoSquare66.svg
+set size=44
+call %inkscapeExe% -f "%svgPath%" -w %size% -h %size% -e "SmallLogo.scale-100.png"
+set size=55
+call %inkscapeExe% -f "%svgPath%" -w %size% -h %size% -e "SmallLogo.scale-125.png"
+set size=66
+call %inkscapeExe% -f "%svgPath%" -w %size% -h %size% -e "SmallLogo.scale-150.png"
+set size=88
+call %inkscapeExe% -f "%svgPath%" -w %size% -h %size% -e "SmallLogo.scale-200.png"
+set size=176
+call %inkscapeExe% -f "%svgPath%" -w %size% -h %size% -e "SmallLogo.scale-400.png"
+
+set svgPath=LogoFill.svg
+set size=50
+call %inkscapeExe% -f "%svgPath%" -w %size% -h %size% -e "StoreLogo.scale-100.png"
+set size=63
+call %inkscapeExe% -f "%svgPath%" -w %size% -h %size% -e "StoreLogo.scale-125.png"
+set size=75
+call %inkscapeExe% -f "%svgPath%" -w %size% -h %size% -e "StoreLogo.scale-150.png"
+set size=100
+call %inkscapeExe% -f "%svgPath%" -w %size% -h %size% -e "StoreLogo.scale-200.png"
+set size=200
+call %inkscapeExe% -f "%svgPath%" -w %size% -h %size% -e "StoreLogo.scale-400.png"
+
+set svgPath=LogoSplash.svg
+call %inkscapeExe% -f "%svgPath%" -w 620 -h 300 -e "SplashScreen.scale-100.png"
+call %inkscapeExe% -f "%svgPath%" -w 775 -h 375 -e "SplashScreen.scale-125.png"
+call %inkscapeExe% -f "%svgPath%" -w 930 -h 450 -e "SplashScreen.scale-150.png"
+call %inkscapeExe% -f "%svgPath%" -w 1240 -h 600 -e "SplashScreen.scale-200.png"
+call %inkscapeExe% -f "%svgPath%" -w 2480 -h 1200 -e "SplashScreen.scale-400.png"
+
+set svgPath=LogoSplash.svg
+call %inkscapeExe% -f "%svgPath%" -w 310 -h 150 -e "LogoWide.scale-100.png"
+call %inkscapeExe% -f "%svgPath%" -w 388 -h 188 -e "LogoWide.scale-125.png"
+call %inkscapeExe% -f "%svgPath%" -w 465 -h 225 -e "LogoWide.scale-150.png"
+call %inkscapeExe% -f "%svgPath%" -w 620 -h 300 -e "LogoWide.scale-200.png"
+call %inkscapeExe% -f "%svgPath%" -w 1240 -h 600 -e "LogoWide.scale-400.png"
