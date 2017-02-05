4addcd319e368755e682f8a298ff347c0f774ec0
Willie Zutz <boarder2@gmail.com>
2017-02-05 15:41:56 -0700

    Implement IDisposable on NotificationManager because it uses a locker.

--- Latest Chatty 8.Store/Managers/NotificationManager.cs
+++ Latest Chatty 8.Store/Managers/NotificationManager.cs
@@ -16,7 +16,7 @@ using Windows.UI.Notifications;
 
 namespace Latest_Chatty_8.Managers
 {
-	public class NotificationManager : BaseNotificationManager
+	public class NotificationManager : BaseNotificationManager, IDisposable
 	{
 		private PushNotificationChannel channel;
 		private LatestChattySettings settings;
@@ -239,6 +239,43 @@ namespace Latest_Chatty_8.Managers
 			{
 				await this.ReRegisterForNotifications();
 			}
+		}
+
+		#endregion
+
+		#region IDisposable Support
+		private bool disposedValue = false; // To detect redundant calls
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!disposedValue)
+			{
+				if (disposing)
+				{
+					// TODO: dispose managed state (managed objects).
+					this.locker.Dispose();
+				}
+
+				// TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+				// TODO: set large fields to null.
+
+				disposedValue = true;
+			}
+		}
+
+		// TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+		// ~NotificationManager() {
+		//   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+		//   Dispose(false);
+		// }
+
+		// This code added to correctly implement the disposable pattern.
+		public void Dispose()
+		{
+			// Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+			Dispose(true);
+			// TODO: uncomment the following line if the finalizer is overridden above.
+			// GC.SuppressFinalize(this);
 		}
 		#endregion
 	}
