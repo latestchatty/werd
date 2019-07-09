8e3b174a6e7391a02fc8a9b2fb4f975384e4ca92
Willie Zutz <boarder2@bit-shift.com>
2019-07-09 16:22:05 -0600

    Change app name to Werd

--- Latest Chatty 8.Store/Properties/AssemblyInfo.cs
+++ Latest Chatty 8.Store/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Latest Chatty")]
+[assembly: AssemblyTitle("Werd")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Bit Shift Software, LLC")]
-[assembly: AssemblyProduct("Latest Chatty")]
+[assembly: AssemblyProduct("Werd")]
 [assembly: AssemblyCopyright("Copyright ©  2012 - 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
