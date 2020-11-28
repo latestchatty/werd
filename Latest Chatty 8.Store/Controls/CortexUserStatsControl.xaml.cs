878f8c94e58d3a1b13f4497509586ca8ed54fd46
Willie Zutz <boarder2@bit-shift.com>
2020-11-27 22:25:30 -0700

    Made usernames clickable to show user info.

--- Latest Chatty 8.Store/Controls/CortexUserStatsControl.xaml.cs
+++ Latest Chatty 8.Store/Controls/CortexUserStatsControl.xaml.cs
@@ -1,22 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.IO;
-using System.Linq;
+﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Werd.DataModel;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
 namespace Werd.Controls
 {
